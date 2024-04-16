3256a8674c5639f680f3d944b620c840e6f2e618
mikurikuri1919 <mikuriyakenta003@gmail.com>
2024-04-16 21:22:01 +0900

    🐛 Imageコンポーネントの追加

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -1,12 +1,16 @@
+import Image from "next/image";
+
 export const Post = () => {
   return (
     <div className="bg-white shadow-md rounded p-4 mb-4">
       <div className="mb-4">
         <div className="flex items-center mb-2">
-          <img
+          <Image
             className="w-10 h-10 rounded-full mr-2"
             src="https://via.placeholder.com/150"
             alt="User Avatar"
+            width={40}
+            height={40}
           />
           <div>
             <h2 className="font-semibold text-md">shincode</h2>
